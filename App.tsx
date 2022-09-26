acdeeb7093487e603d281fbfaacb49c8722e0a5b
juhyeong <juhyeongee@gmail.com>
2022-09-27 04:23:38 +0900

    feat: expo firebase 설치

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBE1-lTShMJL7c7ggeyCo7LH3lS5appIrs",
+    authDomain: "testproductver1.firebaseapp.com",
+    projectId: "testproductver1",
+    storageBucket: "testproductver1.appspot.com",
+    messagingSenderId: "419869963979",
+    appId: "1:419869963979:web:3732c65169575d67bc8d4e",
+    measurementId: "G-6WEB2HM63L",
+  }; // apiKey, authDomain, etc. (see above)
+
+  let myApp = initializeApp(firebaseConfig);
+
+  const sendDataToFirebase = async () => {
+    await setDoc(doc(firestore, "characters", "mario"), {
+      employment: "plumber",
+      outfitColor: "red",
+      specialAttack: "fireball",
+    });
+  };
+
+  const firestore = getFirestore(myApp);
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
